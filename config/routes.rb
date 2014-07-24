68f9ad5ffe8e5650297ed3dbe243dbc9d6d179d2
Yurifrl <yurifrl@hotmail.com>
2014-07-24 19:54:07 -0300

    replace double quotes to single

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'assets/css/*file', to: 'assets#css', format: false
-  get "assets/js/*file", to: 'assets#js', format: false
-  get "assets/image/*file", to: 'assets#img', format: false
+  get 'assets/js/*file', to: 'assets#js', format: false
+  get 'assets/image/*file', to: 'assets#img', format: false
 end
