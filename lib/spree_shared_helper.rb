4ab11db5f6429f848d64a3363d406e9f46eba1cb
Yurifrl <yurifrl@hotmail.com>
2014-07-24 17:36:45 -0300

    update

--- lib/spree_shared_helper.rb
+++ lib/spree_shared_helper.rb
@@ -5,11 +5,16 @@ class SpreeSharedHelper
     else
       db_name = tenant
     end
-    public_global = File.join Rails.root, 'public', 'yebo', db_name
-    public_tenant = File.join Rails.root, 'app', 'tenants', db_name, 'public'
+    tenant = File.join Rails.root, 'app', 'tenants', db_name, 'public'
+    global = File.join Rails.root, 'public', 'yebo', db_name
 
-    FileUtils.mkdir_p public_tenant unless File.exist? public_tenant
-    FileUtils.ln_s public_tenant, public_global, :force => true unless File.exist? public_global
+    FileUtils.mkdir_p tenant unless File.exist? tenant
+    FileUtils.ln_s tenant, global, :force => true unless File.exist? global
+
+    # public_global = File.join Rails.root, 'public', 'yebo', db_name
+    # public_tenant = File.join Rails.root, 'app', 'tenants', db_name, 'public'
+    #FileUtils.mkdir_p public_tenant unless File.exist? public_tenant
+    #FileUtils.ln_s public_tenant, public_global, :force => true unless File.exist? public_global
     ##              #SOURCE,      #DESTINY
   end
 end
