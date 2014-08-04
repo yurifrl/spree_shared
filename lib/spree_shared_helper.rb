49ae0b8d958e148281676e696f56ed6a4f7a69b6
Yurifrl <yurifrl@hotmail.com>
2014-08-04 14:40:47 -0300

    update

--- lib/spree_shared_helper.rb
+++ lib/spree_shared_helper.rb
@@ -5,8 +5,8 @@ class SpreeSharedHelper
     tenant.nil? ? db_name = Apartment::Tenant.current : db_name = tenant
 
     tenant = self.tenants_path db_name
-    yebo   = File.join self.public_path, 'yebo'
-    global = File.join self.public_path, 'yebo', db_name
+    yebo   = "#{self.public_path}/yebo"
+    global = "#{self.public_path}/yebo/#{db_name}"
 
     FileUtils.mkdir_p tenant unless File.exist? tenant
     FileUtils.mkdir_p yebo unless File.exist? yebo
