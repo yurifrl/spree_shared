26c68b3e5ca41f76657210b64d783a8b1de4420d
Yurifrl <yurifrl@hotmail.com>
2014-09-03 15:17:57 -0300

    check if ckeditor exists

--- app/models/ck_editor_decorator.rb
+++ app/models/ck_editor_decorator.rb
@@ -1,34 +1,36 @@
 #Makes image part db specific
-module Ckeditor
-  Picture.try(:class_eval) do
-    # Upload in tenants
-    before_save :confirm_public_folder
+if defined? Ckeditor
+  module Ckeditor
+    Picture.try(:class_eval) do
+      # Upload in tenants
+      before_save :confirm_public_folder
 
-    def confirm_public_folder
-      SpreeSharedHelper.confirm_public_alias_exists
+      def confirm_public_folder
+        SpreeSharedHelper.confirm_public_alias_exists
+      end
+
+      def self.change_paths(tenant)
+        path = SpreeSharedHelper.tenants_path(tenant)
+
+        Ckeditor::Picture.attachment_definitions[:data][:path] = "#{path}/assets/:class/:id/:style/:basename.:extension"
+        Ckeditor::Picture.attachment_definitions[:data][:url]  = "/yebo/#{tenant}/assets/:class/:id/:style/:basename.:extension"
+      end
     end
 
-    def self.change_paths(tenant)
-      path = SpreeSharedHelper.tenants_path(tenant)
+    AttachmentFile.try(:class_eval) do
+      # Upload in tenants
+      before_save :confirm_public_folder
 
-      Ckeditor::Picture.attachment_definitions[:data][:path] = "#{path}/assets/:class/:id/:style/:basename.:extension"
-      Ckeditor::Picture.attachment_definitions[:data][:url]  = "/yebo/#{tenant}/assets/:class/:id/:style/:basename.:extension"
-    end
-  end
+      def confirm_public_folder
+        SpreeSharedHelper.confirm_public_alias_exists
+      end
 
-  AttachmentFile.try(:class_eval) do
-    # Upload in tenants
-    before_save :confirm_public_folder
+      def self.change_paths(tenant)
+        path = SpreeSharedHelper.tenants_path(tenant)
 
-    def confirm_public_folder
-      SpreeSharedHelper.confirm_public_alias_exists
-    end
-
-    def self.change_paths(tenant)
-      path = SpreeSharedHelper.tenants_path(tenant)
-
-      Ckeditor::AttachmentFile.attachment_definitions[:data][:path] = "#{path}/assets/:class/:id/:style/:basename.:extension"
-      Ckeditor::AttachmentFile.attachment_definitions[:data][:url]  = "/yebo/#{tenant}/assets/:class/:id/:style/:basename.:extension"
+        Ckeditor::AttachmentFile.attachment_definitions[:data][:path] = "#{path}/assets/:class/:id/:style/:basename.:extension"
+        Ckeditor::AttachmentFile.attachment_definitions[:data][:url]  = "/yebo/#{tenant}/assets/:class/:id/:style/:basename.:extension"
+      end
     end
   end
 end
